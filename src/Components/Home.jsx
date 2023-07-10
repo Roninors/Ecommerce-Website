615b02dc9623a2cc0a728ca4cd956ff42cc32c7a
ninorslol <likeemnow@gmail.com>
2023-07-11 03:41:15 +0800

    added first collection

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import Hero from "./Home components/Hero";
 import FirstCol from "./Home components/firstCol";
 function Home(){
     return(
-        <div className="overflow-hidden w-[100vw]">
+        <div className="w-[100vw]">
             <Hero/>
             <FirstCol/>
         </div>
