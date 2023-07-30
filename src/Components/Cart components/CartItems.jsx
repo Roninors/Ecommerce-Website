0ec128bea1cd79108f4b627472d482c26400cc8d
ninorslol <likeemnow@gmail.com>
2023-07-30 23:39:31 +0800

    added product total

--- src/Components/Cart components/CartItems.jsx
+++ src/Components/Cart components/CartItems.jsx
@@ -1,56 +1,58 @@
-function CartItems ({userCart}){
+function CartItems({ userCart }) {
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+  return (
+    <div className="flex flex-col p-10 gap-5 w-full">
+      {userCart &&
+        userCart.cart.map((cartItem, i) => (
+          <div
+            className="flex justify-between items-center flex-col p-5 gap-5  lg:flex-row"
+            key={i}
+          >
+            <div className=" w-[40vh] h-[40vh] ">
+              <img
+                src={new URL(cartItem.productImgPath, import.meta.url)}
+                alt=""
+                className=" w-full h-full object-contain object-center"
+              />
+            </div>
 
-        const handleDelete = (id)=>{
-            console.log(id);
-        }
-    return (
-        <div className="flex flex-col p-10 gap-5 w-full">
-            {userCart && userCart.cart.map((cartItem,i)=>(
-                <div className="flex justify-between items-center flex-col p-5 gap-5  lg:flex-row" key={i} >
-                    <div className="w-[40vw] ">
-                    <img  src={new URL(cartItem.productImgPath, import.meta.url)} alt=""  className=" w-full h-full object-contain object-center"/>
-                    </div>
-               
-
-                <div className="flex w-full flex-col justify-between text-center border-b  border-b-gray-300">
-                <div className=" w-full flex flex-col justify-between  items-center font-pop  lg:flex-row ">
-                        <div className=" flex flex-col justify-center items-start">
-                            <h1 className="text-lg">{cartItem.productName}</h1>
-                      
-                            </div>
-
-                            <div className="flex  items-center justify-center my-2 gap-2">
-                                <h1 >Quantity</h1>
-                                <div className="  px-[.5em] border border-gray-300">
-                                    {cartItem.quantity}
-                                </div>
-                            </div>
-
-                            <h1>$ {cartItem.productPrice}</h1>
-                            
-                    
-                   </div>
-
-                  
-                        <div className="w-full flex justify-center items-center my-2  lg:justify-start lg:items-start ">
-                        <p className="text-sm text-gray-500">SHIA</p>
-                       
-                        </div>
-                        <div className="w-full flex flex-col  items-center justify-center  lg:justify-between lg:flex-row lg:mt-20">
-                            <p className="font-pop  text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <img  src={new URL("../../pictures/close.png", import.meta.url)} alt="close"  className="w-5 cursor-pointer my-5 " onClick={()=>handleDelete(cartItem.productId)}/>
-                       
-                        </div>
-                       
+            <div className="flex w-full flex-col justify-between text-center border-b  border-b-gray-300">
+              <div className=" w-full flex flex-col justify-between  items-center font-pop  lg:flex-row ">
+                <div className=" flex flex-col justify-center items-start">
+                  <h1 className="text-lg">{cartItem.productName}</h1>
                 </div>
-           
-                   
-                   </div>
-                
-            ))}
-            
-        </div>
-    )
+
+                <div className="flex  items-center justify-center my-2 gap-2">
+                  <h1>Quantity</h1>
+                  <div className="  px-[.5em] border border-gray-300">
+                    {cartItem.quantity}
+                  </div>
+                </div>
+
+                <h1>$ {cartItem.productPrice * cartItem.quantity}</h1>
+              </div>
+
+              <div className="w-full flex justify-center items-center my-2  lg:justify-start lg:items-start ">
+                <p className="text-sm text-gray-500">SHIA</p>
+              </div>
+              <div className="w-full flex flex-col  items-center justify-center  lg:justify-between lg:flex-row lg:mt-20">
+                <p className="font-pop  text-sm text-gray-500">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </p>
+                <img
+                  src={new URL("../../pictures/close.png", import.meta.url)}
+                  alt="close"
+                  className="w-5 cursor-pointer my-5 "
+                  onClick={() => handleDelete(cartItem.productId)}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default CartItems;
--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,62 +3,70 @@ import { MainContext } from "../context/mainContext";
 import CartItems from "./Cart components/CartItems";
 import { useNavigate } from "react-router-dom";
 
-function Cart(){
+function Cart() {
+  const { token } = useContext(MainContext);
+  const [userCart, setUserCart] = useState(); 
+  const [total,setTotal] = useState(0);
+  const navigate = useNavigate();
 
-    const {token} = useContext(MainContext);
-    const [userCart,setUserCart] = useState();
-    const navigate  = useNavigate();
-        useEffect(()=>{
-            const fetchCart = async()=>{
-                const response = await fetch(`http://localhost:4000/user/${token.id}`)
-                 
-             if (!response.ok) {
-                 throw new Error("failed to fetch specific product");
-               }
-                  const json = await response.json();
-                  setUserCart(json)
-            }
+  useEffect(() => {
+    const fetchCart = async () => {
+      const response = await fetch(`http://localhost:4000/user/${token.id}`);
 
-            fetchCart();
-        },[])
+      if (!response.ok) {
+        throw new Error("failed to fetch specific product");
+      }
 
-      
-    return(
-        <div className="w-full p-16 flex flex-col  items-center justify-center border-t border-b  ">
+      let total = 0;
+      const json = await response.json();
+      json.cart.map((item)=>
+      total += ( item.productPrice * item.quantity)
+     ) 
+      setTotal(total);
+      setUserCart(json);
+    };
 
-                <div className="w-full flex  justify-between flex-col  sm:flex-row     border-b border-b-gray-200">
-                    <div className="flex items-center justify-center gap-2 py-5 ">
+    fetchCart();
+  }, []);
 
-                    <h1 className="font-pop text-lg font-bold">Your Cart</h1>
-                    <p className="font-pop text-sm">3 items</p>
+  
+  
 
-                    </div>
-
-                    <div className="flex items-center justify-center gap-2 mb-5">
-                 
-                    <p className="font-pop text-sm">Total</p>
-                    <h1 className="font-pop text-lg">$ <span className="font-bold">4,190</span></h1>
-            
-                    </div>
-        
-                </div>
-                
-                <CartItems userCart ={userCart} />
-               
-            
-                    <div className="w-full flex justify-between items-center flex-col-reverse gap-5 lg:flex-row">
-                        <div onClick={()=>navigate("/shop")} className=" w-fit flex  justify-between gap-1 items-center flex-col cursor-pointer">
-                        <h1 className="font-pop">Continue Shopping</h1>
-                        <hr className="w-[80%] border-gray-900 "/>
-                        </div>
-
-                        <div ><button  className="bg-black text-base font-pop text-white p-[.5em]">Proceed to checkout</button></div>
-                        
-                    </div>
-              
-               
+return (
+    <div className="w-full p-16 flex flex-col  items-center justify-center border-t border-b  ">
+      <div className="w-full flex  justify-between flex-col  sm:flex-row     border-b border-b-gray-200">
+        <div className="flex items-center justify-center gap-2 py-5 ">
+          <h1 className="font-pop text-lg font-bold">Your Cart</h1>
+          <p className="font-pop text-sm">{userCart && userCart.cart.length} items</p>
         </div>
-    )
+
+        <div className="flex items-center justify-center gap-2 mb-5">
+          <p className="font-pop text-sm">Total</p>
+          <h1 className="font-pop text-lg">
+            $ <span className="font-bold">{total}</span>
+          </h1>
+        </div>
+      </div>
+
+      <CartItems userCart={userCart} />
+
+      <div className="w-full flex justify-between items-center flex-col-reverse gap-5 lg:flex-row">
+        <div
+          onClick={() => navigate("/shop")}
+          className=" w-fit flex  justify-between gap-1 items-center flex-col cursor-pointer"
+        >
+          <h1 className="font-pop">Continue Shopping</h1>
+          <hr className="w-[80%] border-gray-900 " />
+        </div>
+
+        <div>
+          <button className="bg-black text-base font-pop text-white p-[.5em]">
+            Proceed to checkout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
