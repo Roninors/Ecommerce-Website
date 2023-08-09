46b841d5145fda8644267175f6a9540ee018c6d6
Justronin <121565867+Roninors@users.noreply.github.com>
2023-08-09 22:01:37 +0800

    Update useSignup.js

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = ()=>{
 
             setIsLoading(true);
 
-            const response = await fetch("http://localhost:4000/user/signup",{
+            const response = await fetch("https://ecommerce-website-backend-beta.vercel.app/user/signup",{
                 method: "POST",
                 headers: {"Content-Type" : "application/json"},
                 body: JSON.stringify(userDetails)
