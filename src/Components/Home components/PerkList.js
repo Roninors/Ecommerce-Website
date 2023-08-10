54f6f002465a1e36605ec06a3edbdfff713e1ecb
Justronin <121565867+Roninors@users.noreply.github.com>
2023-08-10 22:12:12 +0800

    Update PerkList.js

--- src/Components/Home components/PerkList.js
+++ src/Components/Home components/PerkList.js
@@ -2,22 +2,22 @@ export const PerkList =[
     {
         info: "Safe Logistics",
         about : "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        img: "../../pictures/logistics-delivery.png"
+        img: "https://res.cloudinary.com/dfepnhz2s/image/upload/v1691676631/mmyhotfvx2v7nb8s5xuw.png"
     },
     {
         info: "Refundable",
         about : "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        img: "../../pictures/refund.png"
+        img: "https://res.cloudinary.com/dfepnhz2s/image/upload/v1691676634/sa9szxxywy4lzboos98z.png"
     },
     {
         info: "Free Shipping",
         about : "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        img: "../../pictures/shipped.png"
+        img: "https://res.cloudinary.com/dfepnhz2s/image/upload/v1691676632/tjms29uezyuhwvhgyjjy.png"
     },
     {
         info: "Trusted Seller",
         about : "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-        img: "../../pictures/verified.png"
+        img: "https://res.cloudinary.com/dfepnhz2s/image/upload/v1691676633/vvwflvvpksqn6oxnlwpp.png"
     }
 ]
 
